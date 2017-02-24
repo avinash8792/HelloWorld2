9f54898d0eb5e67bf19635e44a161427717761d5
avinash8792 <avinash8792@gmail.com>
2017-02-24 17:40:17 +0000

    Signed-off-by: avinash8792 <avinash8792@gmail.com>

--- src/com/avi/first/Hello.java
+++ src/com/avi/first/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World ..... From Main class");
+		System.out.println("Made changes");
 		
 		ClassB cb = new ClassB();
 		ClassC cc = new ClassC();
